7add1a71bec19afdf87b5be5a98d323f5ae4d394
Chris Pennington <chris@codinginpublic.dev>
2023-06-05 07:49:24 -0600

    lesson-7 code

--- settings.js
+++ settings.js
@@ -7,7 +7,43 @@ const doc = document.documentElement;
 
 let isAudioPlayable;
 
+// settings with defaults
+const settings = [
+  {
+    key: "sound",
+    default: "false",
+  },
+  {
+    key: "motion",
+    default: "true",
+  },
+  {
+    key: "round",
+    default: "false",
+  },
+  {
+    key: "theme",
+    default: "system",
+  },
+  {
+    key: "customColor",
+    default: "accent2",
+  },
+];
+
 // functions
+function updateSettingsUi({ name, value }) {
+  // boolean toggles
+  if (value === "true" || value === "false") {
+    const checkbox = document.querySelector(`[name="${name}"]`);
+    return (checkbox.checked = value === "true" ? true : false);
+  }
+
+  // all remaining radios
+  const radio = document.querySelector(`#${value}`);
+  return (radio.checked = true);
+}
+
 function updateSiteUi({ name, value }) {
   if (name === "customColor") {
     return doc.style.setProperty("--customColor", `var(--${value})`);
@@ -24,6 +60,17 @@ function playAudio(type) {
 }
 
 // event listeners
+window.addEventListener("DOMContentLoaded", () => {
+  settings.forEach((setting) => {
+    const value = localStorage.getItem(setting.key) ?? setting.default;
+    updateSiteUi({ name: setting.key, value });
+    updateSettingsUi({ name: setting.key, value });
+    if (setting.key === "sound") {
+      isAudioPlayable = value === "true" ? true : false;
+    }
+  });
+});
+
 toggles.forEach((toggle) => {
   toggle.addEventListener("change", (e) => {
     const { name, checked } = e.target;
